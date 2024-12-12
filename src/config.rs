261b6f899aa0e95aed09343d8e2860b0670d5bf0
Gianbelinche <39842759+gianbelinche@users.noreply.github.com>
2024-12-12 15:02:51 -0300

    Sync config

--- src/config.rs
+++ src/config.rs
@@ -27,20 +27,14 @@ pub struct EigenConfig {
     pub eigenda_eth_rpc: String,
     /// Address of the service manager contract
     pub eigenda_svc_manager_address: String,
-    /// Maximun amount of time in milliseconds to wait for a status query response
-    pub status_query_timeout: u64,
-    /// Interval in milliseconds to query the status of a blob
-    pub status_query_interval: u64,
     /// Wait for the blob to be finalized before returning the response
     pub wait_for_finalization: bool,
     /// Authenticated dispersal
     pub authenticated: bool,
-    /// Verify the certificate of dispatched blobs
-    pub verify_cert: bool,
-    /// Path or link to the file containing the points used for KZG
-    pub points_source: PointsSource,
-    /// Chain ID of the Ethereum network
-    pub chain_id: u64,
+    /// Url to the file containing the G1 point used for KZG
+    pub g1_url: String,
+    /// Url to the file containing the G2 point used for KZG
+    pub g2_url: String,
 }
 
 #[derive(Clone, Debug, PartialEq)]
