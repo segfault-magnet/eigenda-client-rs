7e935c13ee21f5fd37d246157bd25fdead5418d7
juan518munoz <62400508+juan518munoz@users.noreply.github.com>
2025-03-20 16:55:37 -0300

    add proto bindings (#33)

--- crates/rust-eigenda-v2-client/src/lib.rs
+++ crates/rust-eigenda-v2-client/src/lib.rs
@@ -3,3 +3,36 @@ pub mod disperser_client;
 pub mod retrieval_client;
 mod utils;
 pub mod verifier;
+
+#[allow(clippy::all)]
+pub(crate) mod generated {
+    pub mod common {
+        include!("generated/common.rs");
+
+        pub mod v2 {
+            include!("generated/common.v2.rs");
+        }
+    }
+
+    pub mod disperser {
+        pub mod v2 {
+            include!("generated/disperser.v2.rs");
+        }
+    }
+
+    pub mod encoder {
+        pub mod v2 {
+            include!("generated/encoder.v2.rs");
+        }
+    }
+
+    pub mod retriever {
+        pub mod v2 {
+            include!("generated/retriever.v2.rs");
+        }
+    }
+
+    pub mod validator {
+        include!("generated/validator.rs");
+    }
+}
